0a7a5057405e255679fc742b0ffd4df52467b6d8
Alessandro <a-cavallo@outlook.com>
2025-03-28 08:14:47 +0000

    Dynamic Text Updates

--- Main.py
+++ Main.py
@@ -1,21 +1,25 @@
-#launching UI and calling drinks
 #_____Imports_____
 from time import sleep
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+import json
+import Drinks #functions containing the instructions for each drink
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'Post'])
 def HomePage():
-    global button_value
-    button_value = None
     if request.method == 'POST':
-        button_value = request.form.get('button_value')
+        session['button_value'] = request.form.get('button_value') #stores button value so that it can be retreved elsewhere
+        print(f"Button Value: {session.get('button_value')}")#debug
     return render_template('HomePage.html')
 
-@app.route('/Preparing')
+@app.route('/Preparing', methods=['GET', 'Post'])
 def Preparing():
-    return render_template('TextPage.html', Text = button_value)
-
+    button_value = request.form.get('button_value') or session.get('button_value')
+    print(f"Button Value: {button_value}")#debug
+    if button_value == "LemonTea":
+        instructions = Drinks.LemonTea()
+        print(f"Instructions: {instructions}") #debug
+    return render_template('TextPage.html', Text = "...", instructionarray = json.dumps(instructions)) #coverts the array to JSON so that it can be used with Javasript in the HTML files
 
 
