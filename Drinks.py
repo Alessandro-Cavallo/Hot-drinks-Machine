eff66667d03bda2793f24e8cb764bc7884b09584
Alessandro <a-cavallo@outlook.com>
2025-03-31 16:45:12 +0100

    Dynamic Text Updates Dynamic Image Updates Coffee Selection Page

--- Drinks.py
+++ Drinks.py
@@ -1,11 +1,34 @@
-def LemonTea():
-    Instructions = ["Boiling Water", "Steeping Tea", "Pouring Tea", "Adding Lemon"]
+#Declare the instructions and images of all the drinks
+#len(Instructions) == len(Images)
+
+def LemonTea_Text():
+    Instructions = ["Boiling Water", "Steeping Tea", "Pouring Tea", "Adding Lemon", "Your drink is ready!"]
     return Instructions
 
-def Coffee():
-    Instructions = ["Boiling Water", "Brewing Coffee Grounds", "Pouring Coffee"] #needs option for adding milk and sugar
-    return Instructions
+def LemonTea_Images():
+    Images = ["Boiling_Water.jpg", "Steeping_Tea.jpg", "Pouring_Tea.jpg", "Adding_Lemon.jpg", "Drink_Ready.jpg"]
+    return Images
 
 def HotChocolate():
-    Instructions = ["Boiling Water", "Adding Chocolate", "Pouring"]
+    Instructions = ["Boiling Water", "Adding Chocolate", "Pouring", "Your drink is ready!"]
     return Instructions
+
+def HotChocolate_Images():
+    Images = ["Boiling_Water.jpg", "Adding Chocolate.jpg", "Pouring_Tea.jpg", "Coffee_Drink_Ready.jpg"]
+    return Images
+
+def Coffee_Americano():
+    Instructions = ["Boiling Water", "Brewing Coffee Grounds", "Pouring Coffee", "Your drink is ready!"]
+    return Instructions
+
+def Coffee_Americano_Images():
+    Images = ["Boiling_Water.jpg", "Brewing_Coffee.jpg", "Pouring_Coffee.jpg", "Coffee_Drink_Ready.jpg"]
+    return Images
+
+def Coffee_Latte():
+    Instructions = ["Boiling Water", "Brewing Coffee Grounds", "Pouring Coffee", "Pouring Milk", "Your drink is ready!"]
+    return Instructions
+
+def Coffee_Latte():
+    Images = ["Boiling_Water.jpg", "Brewing_Coffee.jpg", "Pouring_Coffee.jpg", "Pouring Milk.jpg" "Coffee_Drink_Ready.jpg"]
+    return Images
--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 #_____Imports_____
 from time import sleep
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, url_for
 import json
 import Drinks #functions containing the instructions for each drink
 
@@ -15,11 +15,27 @@ def HomePage():
 
 @app.route('/Preparing', methods=['GET', 'Post'])
 def Preparing():
+    instructions = ["No instructions Found"]
     button_value = request.form.get('button_value') or session.get('button_value')
     print(f"Button Value: {button_value}")#debug
     if button_value == "LemonTea":
-        instructions = Drinks.LemonTea()
+        instructions = Drinks.LemonTea_Text()
+        images = [url_for('static', filename=f'Images/{img}') for img in Drinks.LemonTea_Images()]
         print(f"Instructions: {instructions}") #debug
-    return render_template('TextPage.html', Text = "...", instructionarray = json.dumps(instructions)) #coverts the array to JSON so that it can be used with Javasript in the HTML files
+        
+    elif button_value == "HotChocolate":
+        instructions = Drinks.HotChocolate()
+        images = [url_for('static', filename=f'Images/{img}') for img in Drinks.HotChocolate_Images()]
+    
+    elif button_value == "Coffee":
+        return render_template('Coffees.html')
+    
+    elif button_value == "Coffee_Americano":
+        instructions = Drinks.Coffee_Americano()
+        images = [url_for('static', filename=f'Images/{img}') for img in Drinks.Coffee_Americano_Images()]
+
+    return render_template('TextPage.html', Text = "...", instructionarray = json.dumps(instructions), imagearray = json.dumps(images)) #coverts the array to JSON so that it can be used with Javasript in the HTML files
+    
+
 
 
