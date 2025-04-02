6340d2f0ee0f2d649ae87c20c85a25d42254a625
Alessandro <a-cavallo@outlook.com>
2025-04-02 10:15:34 +0100

    Changes elif statements into switch case statements

--- Main.py
+++ Main.py
@@ -9,36 +9,34 @@ app = Flask(__name__)
 def HomePage():
     if request.method == 'POST':
         session['button_value'] = request.form.get('button_value') #stores button value so that it can be retreved elsewhere
-        # print(f"Button Value: {session.get('button_value')}")#debug
     return render_template('HomePage.html')
 
 @app.route('/Preparing', methods=['GET', 'Post'])
 def Preparing():
     instructions = ["No instructions Found"]
     button_value = request.form.get('button_value') or session.get('button_value')
-    # print(f"Button Value: {button_value}")#debug
-    if button_value == "LemonTea":
-        instructions = Drinks.LemonTea_Text()
-        images = [url_for('static', filename=f'Images/{img}') for img in Drinks.LemonTea_Images()]
-        # print(f"Instructions: {instructions}") #debug
+    match button_value: #match case works like a set of if and elif statements
+        case "LemonTea":
+            instructions = Drinks.LemonTea_Text()
+            images = [url_for('static', filename=f'Images/{img}') for img in Drinks.LemonTea_Images()]
         
-    elif button_value == "HotChocolate":
-        instructions = Drinks.HotChocolate()
-        images = [url_for('static', filename=f'Images/{img}') for img in Drinks.HotChocolate_Images()]
+        case "HotChocolate":
+            instructions = Drinks.HotChocolate()
+            images = [url_for('static', filename=f'Images/{img}') for img in Drinks.HotChocolate_Images()]
     
-    elif button_value == "Coffee":
-        return render_template('Coffees.html')
+        case "Coffee":
+            return render_template('Coffees.html')
     
-    elif button_value == "Home":
-        return render_template('HomePage.html')
-    
-    elif button_value == "Coffee_Americano":
-        instructions = Drinks.Coffee_Americano()
-        images = [url_for('static', filename=f'Images/{img}') for img in Drinks.Coffee_Americano_Images()]
-    
-    elif button_value == "Coffee_Latte":
-        instructions = Drinks.Coffee_Latte()
-        images = [url_for('static', filename=f'Images/{img}') for img in Drinks.Coffee_Latte_images()]
+        case "Home":
+            return render_template('HomePage.html')
+        
+        case "Coffee_Americano":
+            instructions = Drinks.Coffee_Americano()
+            images = [url_for('static', filename=f'Images/{img}') for img in Drinks.Coffee_Americano_Images()]
+        
+        case "Coffee_Latte":
+            instructions = Drinks.Coffee_Latte()
+            images = [url_for('static', filename=f'Images/{img}') for img in Drinks.Coffee_Latte_images()]
 
     return render_template('TextPage.html', Text = "...", instructionarray = json.dumps(instructions), imagearray = json.dumps(images)) #coverts the array to JSON so that it can be used with Javasript in the HTML files
     
